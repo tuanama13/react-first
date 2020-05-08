8c6949f9494757f3392dff6f7f84ba6541370df3
Ama <tuanama7@gmail.com>
2020-05-08 23:57:13 +0700

    Added First React Project

---
+++ App.js
@@ -0,0 +1,41 @@
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, FlatList} from 'react-native';
+import Header from './components/Header';
+import ListItem from './components/ListItem';
+import 'react-native-get-random-values';
+import {v4 as uuidv4} from 'uuid';
+
+const App = () => {
+  const [items, setItems] = useState([
+    {id: uuidv4(), text: 'Telur'},
+    {id: uuidv4(), text: 'Susu'},
+    {id: uuidv4(), text: 'Indomie'},
+    {id: uuidv4(), text: 'Kue'},
+  ]);
+
+  const deleteItem = (id) => {
+    setItems((prevItems) => {
+      return prevItems.filter((item) => item.id != id);
+    });
+  };
+
+  return (
+    <View style={styles.container}>
+      <Header />
+      <FlatList
+        data={items}
+        renderItem={({item}) => (
+          <ListItem item={item} deleteItem={deleteItem} />
+        )}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default App;
