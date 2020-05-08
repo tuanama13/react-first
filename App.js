a7cc95a55bc0acb8cc03c68b9d1edb1546282600
tuanama13 <tuanama7@gmail.com>
2020-05-09 00:52:02 +0700

    Add Function Add Items

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import React, {useState} from 'react';
-import {View, Text, StyleSheet, FlatList} from 'react-native';
+import {View, Text, StyleSheet, FlatList, Alert} from 'react-native';
 import Header from './components/Header';
 import ListItem from './components/ListItem';
+import AddItem from './components/AddItem';
 import 'react-native-get-random-values';
 import {v4 as uuidv4} from 'uuid';
 
@@ -15,13 +16,24 @@ const App = () => {
 
   const deleteItem = (id) => {
     setItems((prevItems) => {
-      return prevItems.filter((item) => item.id != id);
+      return prevItems.filter((item) => item.id !== id);
     });
+  };
+
+  const addItem = (text) => {
+    if (!text) {
+      Alert.alert('Error', 'Masukan Item Dahulu!');
+    } else {
+      setItems((prevItems) => {
+        return [{id: uuidv4(), text}, ...prevItems];
+      });
+    }
   };
 
   return (
     <View style={styles.container}>
       <Header />
+      <AddItem addItem={addItem} />
       <FlatList
         data={items}
         renderItem={({item}) => (
